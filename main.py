c5fcb92dc09c13220fea3bf4f08ed770aaead319
ZassiM <zarotto_raz@live.it>
2022-04-25 16:50:34 +0200

    "before change WL"

--- main.py
+++ main.py
@@ -1,79 +1,84 @@
-from src.netlist_gen import netlist_design
+'''
+    Main python file
+'''
 import json
 import csv
 import os
-import time
+#import time
 import sys
+from src.netlist_gen import netlist_design
 
-config_filepath = "config.json"
-pulses_input_filepath = "cell_input.csv"
-row_input_filepath = "row_input.csv"
-out_file_name = "netlist.scs" 
-memristor_model_path = os.getcwd() + '/deps/memristor-models/' 
+CONFIG_FILEPATH = "config.json"
+PULSES_INPUT_FILEPATH = "cell_input.csv"
+ROW_INPUT_FILEPATH = "row_input.csv"
+OUT_FILE_NAME = "netlist.scs"
+memristor_model_path = os.getcwd() + '/deps/memristor-models/'
 transistor_model_path = os.getcwd() + '/deps/transistor-models/'
-circuit = netlist_design()  
+circuit = netlist_design()
 
-mean_sigma = { "Ndiscmin": (8e-03, 2e-3), "Ndiscmax": (20, 1), "lnew": (0.4, 0.04), "rnew": (45e-09, 5e-09)}	
+mean_sigma = {"Ndiscmin": (8e-03, 2e-3), "Ndiscmax": (20, 1), "lnew": (0.4, 0.04), "rnew": (45e-09, 5e-09)}	
 
 memristor_params = {}
 
 print("\n***********************************************")
 
 if len(sys.argv) > 1 and str(sys.argv[1]) == 'sample':
-	config_filepath = "sample/" + config_filepath
-	pulses_input_filepath = "sample/" + pulses_input_filepath
-	row_input_filepath = "sample/" + row_input_filepath
-	print("Sample used.\n")
+    CONFIG_FILEPATH = "sample/" + CONFIG_FILEPATH
+    PULSES_INPUT_FILEPATH = "sample/" + PULSES_INPUT_FILEPATH
+    ROW_INPUT_FILEPATH = "sample/" + ROW_INPUT_FILEPATH
+    print("Sample used.\n")
 
 
-with open(config_filepath, 'r') as file:
-	config = json.load(file)
 
-	input_type = config['input_type']['row_by_row']
+with open(CONFIG_FILEPATH, 'r', encoding="utf-8") as file:
+    config = json.load(file)
 
-	sim_params = config["sim_params"]
-	sim_type, step_time, period, max_step, time_units = sim_params['type'], sim_params['step_time'], sim_params['period'], sim_params['max_step'],sim_params['time_units']
-	vabstol, iabstol, temp, tnom, gmin = sim_params['vabstol'], sim_params['iabstol'], sim_params['temp'], sim_params['tnom'], sim_params['gmin']
+    input_type = config['input_type']['row_by_row']
 
-	crossbar_params = config["crossbar_params"]
+    sim_params = config["sim_params"]
+    sim_type, step_time, period, max_step, time_units = sim_params['type'], sim_params['step_time'], sim_params['period'], sim_params['max_step'],sim_params['time_units']
 
-	memristor_model_path += crossbar_params['memristor_model_file']
-	transistor_model_path += crossbar_params['transistor_model_file']
-	read_v, set_v, reset_v, gate_v = crossbar_params['read_v'], crossbar_params['set_v'], crossbar_params['reset_v'], crossbar_params['gate_v']
-	nmin_b, nmax_b, ldet_b, rdet_b = crossbar_params['ndiscmin'], crossbar_params['ndiscmax'], crossbar_params['ldet'], crossbar_params['rdet']	
-	transistor_lenght, transistor_width = crossbar_params["transistor_length"], crossbar_params["transistor_width"]
+    vabstol, iabstol, temp, tnom, gmin = sim_params['vabstol'], sim_params['iabstol'], sim_params['temp'], sim_params['tnom'], sim_params['gmin']
 
-	crossbar_params = list(crossbar_params.items())
+    crossbar_params = config["crossbar_params"]
 
-	if(len(crossbar_params) > 12):
-		for i in range(12, len(crossbar_params)):
-			memristor_params[crossbar_params[i][0]] = crossbar_params[i][1]
-			#Rth0 = 1.572e7
+    memristor_model_path += crossbar_params['memristor_model_file']
+    transistor_model_path += crossbar_params['transistor_model_file']
+    read_v, set_v, reset_v, gate_v = crossbar_params['read_v'], crossbar_params['set_v'], crossbar_params['reset_v'], crossbar_params['gate_v']
+    nmin_b, nmax_b, ldet_b, rdet_b = crossbar_params['ndiscmin'], crossbar_params['ndiscmax'], crossbar_params['ldet'], crossbar_params['rdet']	
+    transistor_lenght, transistor_width = crossbar_params["transistor_length"], crossbar_params["transistor_width"]
+
+    crossbar_params = list(crossbar_params.items())
+
+    if len(crossbar_params) > 12:
+        for i in range(12, len(crossbar_params)):
+            memristor_params[crossbar_params[i][0]] = crossbar_params[i][1]
+            #Rth0 = 1.572e7
 
 
-	gate_sweep = config['gate_sweep']
-	if gate_sweep['min_v'] == 0 and gate_sweep['max_v'] == 0:
-		sweep_params = []
-	else:
-		min_gate = gate_sweep['min_v']
-		max_gate = gate_sweep['max_v']
-		step_gate = gate_sweep['step_v']
-		sweep_params = [min_gate, max_gate, step_gate]		
+    gate_sweep = config['gate_sweep']
+    if gate_sweep['min_v'] == 0 and gate_sweep['max_v'] == 0:
+        sweep_params = []
+    else:
+        min_gate = gate_sweep['min_v']
+        max_gate = gate_sweep['max_v']
+        step_gate = gate_sweep['step_v']
+        sweep_params = [min_gate, max_gate, step_gate]		
 
 
 if input_type == 1:
-	with open(row_input_filepath, 'r') as file:
-		reader = csv.reader(file)
-		row_pulses_list = list(filter(None,reader))
-		circuit.pulses_to_file(row_pulses_list, pulses_input_filepath)
+    with open(ROW_INPUT_FILEPATH, 'r', encoding="utf-8") as file:
+        reader = csv.reader(file)
+        row_pulses_list = list(filter(None,reader))
+        circuit.pulses_to_file(row_pulses_list, PULSES_INPUT_FILEPATH)
 
-with open(pulses_input_filepath, 'r') as file:
-		reader = csv.reader(file)
-		in_pulses_list = list(filter(None,reader))
+with open(PULSES_INPUT_FILEPATH, 'r', encoding="utf-8") as file:
+    reader = csv.reader(file)
+    in_pulses_list = list(filter(None,reader))
 
 if not in_pulses_list:
-	print("No pulses input! Terminating program...")
-	quit()
+    print("No pulses input! Terminating program...")
+    sys.exit()
 
 
 circuit.calculate_xbar_size(in_pulses_list)
@@ -83,10 +88,10 @@ circuit.set_simulation_params(sim_type, step_time, period, max_step, time_units,
 var_bools = circuit.set_variablity(Nmin = nmin_b, Nmax = nmax_b, ldet = ldet_b, rdet = rdet_b)	
 var_param = circuit.update_param(mean_sigma, var_bools)	
 
-circuit.gen_netlist(memristor_params, in_pulses_list, sweep_params, out_file_name, memristor_model_path, transistor_model_path)	
+circuit.gen_netlist(memristor_params, in_pulses_list, sweep_params, OUT_FILE_NAME, memristor_model_path, transistor_model_path)	
 
 # input("Press Enter to run the simulation.\n")
 # print("Running simulation...")
 # print("***********************************************\n")
 # time.sleep(1.5)
-# os.system(f"spectre {out_file_name}")
+# os.system(f"spectre {OUT_FILE_NAME}")
--- src/netlist_params.py
+++ src/netlist_params.py
@@ -2,174 +2,174 @@ import copy
 
 class parameters(object):  
 
-	def __init__(self, device="det", simulation = "spectre"): 
-		
-		if simulation !="spectre":
-			print("simulation needs to be set = spectre")
-			exit()
-		if device =="var":
-			self.memristor_model= "JART_VCM_1b_VAR"
-		else:
-			self.memristor_model= "JART_VCM_1b_det"
-		
-		self.transistor_model = "nmos"
+    def __init__(self, device="det", simulation = "spectre"): 
+        
+        if simulation !="spectre":
+            print("simulation needs to be set = spectre")
+            exit()
+        if device =="var":
+            self.memristor_model= "JART_VCM_1b_VAR"
+        else:
+            self.memristor_model= "JART_VCM_1b_det"
+        
+        self.transistor_model = "nmos"
 
 
-	def parameters_list(self, param= {}, model_name="det", numerical_mode = True):
+    def parameters_list(self, param, model_name="det", numerical_mode = True):
 
-		self.device_parameters = ""
-		k = 0
-		if model_name=="var" or model_name == "det":
-			for i in param:
-				k += 1
-				if numerical_mode:
-					self.device_parameters += i + " = " + str(param[i]) + " "   
-				else:
-					self.device_parameters += i + " = " + i + " " 
-				if k == 5:
-					self.device_parameters += "\\\n\t\t"
-					k=0
-		
-		return self.device_parameters
-
-
-	def variablity_param(self, iteration):
-
-		variablity_dict = {}
-		variablity_dict["Ninit"] = "Ndiscmin"
-
-		if self.vary_nmin:
-			ndisc_min_var = f"Ndiscmin{iteration} "
-			variablity_dict["Ndiscmin"] = variablity_dict["Ninit"] = ndisc_min_var
-		if self.vary_nmax:
-			ndisc_max_var = f"Ndiscmax{iteration} "
-			variablity_dict["Ndiscmax"] = ndisc_max_var
-		if self.vary_ldet:
-			lnew_var = f"lnew{iteration} "
-			variablity_dict["lnew"] = lnew_var
-		if self.vary_rdet:
-			rnew_var = f"rnew{iteration}"
-			variablity_dict["rnew"] = rnew_var
-
-		return variablity_dict
-
-	def set_input_voltages(self, volt_r = [], volt_c = []):
-
-		self.input_type_r, self.input_type_c = [], []
-		self.volt_0_r, self.volt_0_c = [], []
-		self.volt_1_r, self.volt_1_c = [], []
-		self.time_period_r, self.time_period_c = [], []
-		self.pulse_width_r, self.pulse_width_c = [], []
-		self.rise_time_r, self.rise_time_c = [], []
-		self.fall_time_r, self.fall_time_c = [], []
-
-		def_vol = ["pulse", 0, 0, "0", "0", "0", "0"]
-
-		if self.rows < len(volt_r):
-			print(f"There are {len(volt_r)} voltage pulses but only {self.rows} rows -> {len(volt_r)-self.rows} voltage pulses are ignored.\n")
-			while self.rows != len(volt_r):
-				volt_r.pop()
-		
-		if self.columns < len(volt_c):
-			print(f"There are {len(volt_c)} voltage pulses but only {self.columns} columns -> {len(volt_c)-self.columns} voltage pulses are ignored.\n")
-			while self.columns != len(volt_c):
-				volt_c.pop()
-		
-		if self.rows > len(volt_r):
-			print(f"There are {self.rows} rows, but only {len(volt_r)} voltage pulses are defined -> {self.rows - len(volt_r)} null voltages are added.\n")
-			while self.rows != len(volt_r):
-				volt_r.append(def_vol)
-		
-		if self.columns > len(volt_c):
-			print(f"There are {self.columns} columns, but only {len(volt_c)} voltage pulses are defined -> {self.columns - len(volt_c)} null voltages are added.\n")
-			while self.columns != len(volt_c):
-				volt_c.append(def_vol)
-
-		for v in volt_r:
-			self.input_type_r.append(v[0])
-			self.volt_0_r.append(v[1])
-			self.volt_1_r.append(v[2])
-			self.time_period_r.append(v[3])
-			self.pulse_width_r.append(v[4])
-			self.rise_time_r.append(v[5])
-			self.fall_time_r.append(v[6])
-
-		for v in volt_c:
-			self.input_type_c.append(v[0])
-			self.volt_0_c.append(v[1])
-			self.volt_1_c.append(v[2])
-			self.time_period_c.append(v[3])
-			self.pulse_width_c.append(v[4])
-			self.rise_time_c.append(v[5])
-			self.fall_time_c.append(v[6])
-		
-		print("Voltage pulses correctly added.\n")
+        self.device_parameters = ""
+        k = 0
+        if model_name=="var" or model_name == "det":
+            for i in param:
+                k += 1
+                if numerical_mode:
+                    self.device_parameters += i + " = " + str(param[i]) + " "   
+                else:
+                    self.device_parameters += i + " = " + i + " " 
+                if k == 5:
+                    self.device_parameters += "\\\n\t\t"
+                    k=0
+        
+        return self.device_parameters
 
 
-	def set_simulation_params(self, type_ = "tran", step_time = 100, period = 100, max_step = 1, time_units = "u", vabstol = "1e-6", iabstol = "1e-12", temp = "27", tnom = "27", gmin = "1e-12", in_pulses_list = []):
+    def variablity_param(self, iteration):
 
-		self.simulation_type = type_
-		self.time_units = time_units
-		self.simulation_maxstep = str(max_step) + self.time_units
-		self.step_time = step_time
-		if period < 2 : period = 2
-		self.period = period
-		self.simulation_stop_time = str(self.calculate_stop_time(in_pulses_list)) + self.time_units
-		self.vabstol, self.iabstol, self.temp, self.tnom, self.gmin = vabstol, iabstol, temp, tnom, gmin
+        variablity_dict = {}
+        variablity_dict["Ninit"] = "Ndiscmin"
 
-		print(f"Stop time: {self.simulation_stop_time}s, Step time: {self.step_time}{self.time_units}s, Max step: {self.simulation_maxstep}s.\n")
+        if self.vary_nmin:
+            ndisc_min_var = f"Ndiscmin{iteration} "
+            variablity_dict["Ndiscmin"] = variablity_dict["Ninit"] = ndisc_min_var
+        if self.vary_nmax:
+            ndisc_max_var = f"Ndiscmax{iteration} "
+            variablity_dict["Ndiscmax"] = ndisc_max_var
+        if self.vary_ldet:
+            lnew_var = f"lnew{iteration} "
+            variablity_dict["lnew"] = lnew_var
+        if self.vary_rdet:
+            rnew_var = f"rnew{iteration}"
+            variablity_dict["rnew"] = rnew_var
 
-	def calculate_xbar_size(self, in_pulses_list = []):
+        return variablity_dict
 
-		rows = 0
-		columns = 0
+    def set_input_voltages(self, volt_r = [], volt_c = []):
 
-		for s in in_pulses_list:
-			
-			row,column = int(s[1]), int(s[2])
-			
-			if(row > rows): rows = row
-			if(column > columns): columns = column
+        self.input_type_r, self.input_type_c = [], []
+        self.volt_0_r, self.volt_0_c = [], []
+        self.volt_1_r, self.volt_1_c = [], []
+        self.time_period_r, self.time_period_c = [], []
+        self.pulse_width_r, self.pulse_width_c = [], []
+        self.rise_time_r, self.rise_time_c = [], []
+        self.fall_time_r, self.fall_time_c = [], []
 
-		self.rows = rows + 1
-		self.columns = columns + 1
-		
-		print(f"Crossbar size: {self.rows} rows, {self.columns} columns.\n")
+        def_vol = ["pulse", 0, 0, "0", "0", "0", "0"]
+
+        if self.rows < len(volt_r):
+            print(f"There are {len(volt_r)} voltage pulses but only {self.rows} rows -> {len(volt_r)-self.rows} voltage pulses are ignored.\n")
+            while self.rows != len(volt_r):
+                volt_r.pop()
+        
+        if self.columns < len(volt_c):
+            print(f"There are {len(volt_c)} voltage pulses but only {self.columns} columns -> {len(volt_c)-self.columns} voltage pulses are ignored.\n")
+            while self.columns != len(volt_c):
+                volt_c.pop()
+        
+        if self.rows > len(volt_r):
+            print(f"There are {self.rows} rows, but only {len(volt_r)} voltage pulses are defined -> {self.rows - len(volt_r)} null voltages are added.\n")
+            while self.rows != len(volt_r):
+                volt_r.append(def_vol)
+        
+        if self.columns > len(volt_c):
+            print(f"There are {self.columns} columns, but only {len(volt_c)} voltage pulses are defined -> {self.columns - len(volt_c)} null voltages are added.\n")
+            while self.columns != len(volt_c):
+                volt_c.append(def_vol)
+
+        for v in volt_r:
+            self.input_type_r.append(v[0])
+            self.volt_0_r.append(v[1])
+            self.volt_1_r.append(v[2])
+            self.time_period_r.append(v[3])
+            self.pulse_width_r.append(v[4])
+            self.rise_time_r.append(v[5])
+            self.fall_time_r.append(v[6])
+
+        for v in volt_c:
+            self.input_type_c.append(v[0])
+            self.volt_0_c.append(v[1])
+            self.volt_1_c.append(v[2])
+            self.time_period_c.append(v[3])
+            self.pulse_width_c.append(v[4])
+            self.rise_time_c.append(v[5])
+            self.fall_time_c.append(v[6])
+        
+        print("Voltage pulses correctly added.\n")
 
 
-	def calculate_stop_time(self, in_pulses_list):
-		max_stop_time = 0
-		cnt = 0
-		for i in range(0, self.rows):
-			for s in in_pulses_list:
-				if int(s[1]) == i:
-					cnt += 1
-			stop_time = (self.step_time + self.period)*cnt + self.step_time
-			if stop_time > max_stop_time : max_stop_time = stop_time
+    def set_simulation_params(self, type_ = "tran", step_time = 100, period = 100, max_step = 1, time_units = "u", vabstol = "1e-6", iabstol = "1e-12", temp = "27", tnom = "27", gmin = "1e-12", in_pulses_list = []):
 
-			cnt = 0
+        self.simulation_type = type_
+        self.time_units = time_units
+        self.simulation_maxstep = str(max_step) + self.time_units
+        self.step_time = step_time
+        if period < 2 : period = 2
+        self.period = period
+        self.simulation_stop_time = str(self.calculate_stop_time(in_pulses_list)) + self.time_units
+        self.vabstol, self.iabstol, self.temp, self.tnom, self.gmin = vabstol, iabstol, temp, tnom, gmin
 
-		return max_stop_time * self.rows
+        print(f"Stop time: {self.simulation_stop_time}s, Step time: {self.step_time}{self.time_units}s, Max step: {self.simulation_maxstep}s.\n")
+
+    def calculate_xbar_size(self, in_pulses_list = []):
+
+        rows = 0
+        columns = 0
+
+        for s in in_pulses_list:
+            
+            row,column = int(s[1]), int(s[2])
+            
+            if(row > rows): rows = row
+            if(column > columns): columns = column
+
+        self.rows = rows + 1
+        self.columns = columns + 1
+        
+        print(f"Crossbar size: {self.rows} rows, {self.columns} columns.\n")
 
 
-	def set_crossbar_params(self, read_v = 0.2, set_v = -1.05, reset_v = 0.75, gate_v = 1, trans_length = 32, trans_width = 32):
+    def calculate_stop_time(self, in_pulses_list):
+        max_stop_time = 0
+        cnt = 0
+        for i in range(0, self.rows):
+            for s in in_pulses_list:
+                if int(s[1]) == i:
+                    cnt += 1
+            stop_time = (self.step_time + self.period)*cnt + self.step_time
+            if stop_time > max_stop_time : max_stop_time = stop_time
 
-		self.read_v = read_v
-		self.set_v = set_v
-		self.reset_v = reset_v
-		self.gate_v = gate_v
-		self.trans_length = trans_length
-		self.trans_width = trans_width
-		
+            cnt = 0
 
-	def set_variablity(self, Nmin=False, Nmax=False, rdet=False, ldet=False):
+        return max_stop_time * self.rows
 
-		self.vary_nmin = Nmin
-		self.vary_nmax = Nmax
-		self.vary_rdet = rdet
-		self.vary_ldet = ldet
 
-		return {"Ndiscmin": Nmin, "Ndiscmax": Nmax, "rnew": rdet,"lnew": ldet}
+    def set_crossbar_params(self, read_v = 0.2, set_v = -1.05, reset_v = 0.75, gate_v = 1, trans_length = 32, trans_width = 32):
+
+        self.read_v = read_v
+        self.set_v = set_v
+        self.reset_v = reset_v
+        self.gate_v = gate_v
+        self.trans_length = trans_length
+        self.trans_width = trans_width
+        
+
+    def set_variablity(self, Nmin=False, Nmax=False, rdet=False, ldet=False):
+
+        self.vary_nmin = Nmin
+        self.vary_nmax = Nmax
+        self.vary_rdet = rdet
+        self.vary_ldet = ldet
+
+        return {"Ndiscmin": Nmin, "Ndiscmax": Nmax, "rnew": rdet,"lnew": ldet}
 
 
 
